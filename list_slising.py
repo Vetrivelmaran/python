9634d555639fedc31fa25edf1ba543c2d8004b2d
Vetrivelmaran <vetri4723@gmai.com>
2023-09-29 22:47:23 +0530

    i practice some functions

--- list_slising.py
+++ list_slising.py
@@ -1,19 +1,5 @@
-class fibonacci:
-    
-    def __init__(self,n) -> None:
-        self.n = n
-    def fib(self):
-        a =0
-        b = 1
-        for i in range(0,self.n):
-            if i<=1:
-                result = i
-            else:
-                result = a + b
-                a = b
-                b = result
-            print(result)
-ob = fibonacci(10)
-ob.fib()
-
-
+from modul import *
+from think_python import function1
+print(add(12,23))
+print(sub(2,3))
+function1.MyFunction()
---
+++ think_python/function1.py
@@ -0,0 +1,76 @@
+#                                                      Exercise 3-1.
+# Write a function named right_justify that takes a string named s as a parameter
+# and prints the string with enough leading spaces so that the last letter of the string is
+# in column 70 of the display:
+
+"""def right_justify(s):
+    print(' '*(70-len(s))+s)
+right_justify('vetri')"""
+
+
+#                                                       Exercise 3-3.
+
+
+
+# Note: This exercise should be done using only the statements and other features we
+# have learned so far.
+# 1. Write a function that draws a grid like the following:
+# print('+','-'*4,'+','-'*4,'+')
+# print('|'+('  '*3)+'|'+('  '*3)+'|')
+# print('|'+('  '*3)+'|'+('  '*3)+'|')
+# print('|'+('  '*3)+'|'+('  '*3)+'|')
+# print('|'+('  '*3)+'|'+('  '*3)+'|')
+# print('+','-'*4,'+','-'*4,'+')
+# print('|'+('  '*3)+'|'+('  '*3)+'|')
+# print('|'+('  '*3)+'|'+('  '*3)+'|')
+# print('|'+('  '*3)+'|'+('  '*3)+'|')
+# print('|'+('  '*3)+'|'+('  '*3)+'|')
+# print('+','-'*4,'+','-'*4,'+')
+
+# print('\n')
+# def cell():
+#     print('+','-'*4,'+')
+#     print('|'+('  '*3)+'|')
+#     print('|'+('  '*3)+'|')
+#     print('|'+('  '*3)+'|')
+#     print('|'+('  '*3)+'|')
+#     print('+','-'*4,'+')
+    # return 1
+    
+
+# def grid(row,column):
+#     count=0
+#     for i in range(row):
+#         cell()
+#         count+=1
+#         if count==row:
+#             for j in range(column):
+#              cell()
+    
+# row=int(input('enetr the row:'))
+# column =int(input('enter the column:'))
+# grid(row,column)
+    
+
+# def grid():
+#     for i in range(2):
+#             print('+','-'*4,'+')
+#             print('|'+('  '*3)+'|')
+#             print('|'+('  '*3)+'|')
+#             print('|'+('  '*3)+'|')
+#             # print('|'+('  '*3)+'|')
+#             # print('+','-'*4,'+')
+#         # print(end='')
+#     # for i in range(2):
+#         # cell()
+#         # print(end='')
+# # grid()
+
+        
+# for i in range(11):
+#     if i%5==0:
+#       print('+','-'*4,'+','-'*4,'+')
+#     else:                                                 ----------------> ok
+#       print('|'+('  '*3)+'|'+('  '*3)+'|')
+def MyFunction():
+    print('this is other modual function')
